a99bb359ef7cd845088c90fd517ee09d9e5f77f3
Vlad <vlad086367@gmail.com>
2026-05-17 20:04:05 +0300

    Refactor query-string

--- client/src/api/rest/restController.js
+++ client/src/api/rest/restController.js
@@ -1,3 +1,4 @@
+import queryString from 'query-string';
 import http from '../interceptor';
 
 export const registerRequest = data => http.post('registration', data);
@@ -37,12 +38,7 @@ export const changeCatalogName = data => http.post('updateNameCatalog', data);
 //   );
 
 export const getCustomersContests = data =>
-  http.get(
-    console.log(
-      'data :>> ',
-      data
-    )`contests/byCustomer?limit=${data.limit}&offset=${data.offset}&status=${data.contestStatus}`
-  );
+  http.get(`contests/byCustomer?${queryString.stringify(data)}`);
 
 export const getActiveContests = ({
   offset,
