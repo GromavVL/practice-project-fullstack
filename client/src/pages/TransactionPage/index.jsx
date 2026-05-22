f8607edbd423ef0a95d8b7243c84342930d2f5a2
Vlad <vlad086367@gmail.com>
2026-05-22 17:37:54 +0300

    Add page transaction

---
+++ client/src/pages/TransactionPage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+
+function Transaction () {
+  return (
+    <>
+      <Header />
+      Transaction
+    </>
+  );
+}
+
+export default Transaction;
