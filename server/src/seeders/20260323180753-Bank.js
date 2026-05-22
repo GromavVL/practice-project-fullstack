48ef052a757895de9c84f1dac007639737661137
Vlad <vlad086367@gmail.com>
2026-05-22 16:40:07 +0300

    Add new date in bank

--- server/src/seeders/20260323180753-Bank.js
+++ server/src/seeders/20260323180753-Bank.js
@@ -6,14 +6,14 @@ module.exports = {
         {
           cardNumber: '4564654564564564',
           name: 'SquadHelp',
-          expiry: '11/22',
+          expiry: '11/27',
           cvc: '453',
           balance: 0,
         },
         {
           cardNumber: '4111111111111111',
           name: 'yriy',
-          expiry: '09/25',
+          expiry: '09/27',
           cvc: '505',
           balance: 5000,
         },
