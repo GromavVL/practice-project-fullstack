98ec4b42eeba258ea66ed19387156c61465c5f6f
Vlad <vlad086367@gmail.com>
2026-05-23 11:32:23 +0300

    GET / getTransaction for client

--- client/src/api/rest/restController.js
+++ client/src/api/rest/restController.js
@@ -61,3 +61,5 @@ export const getActiveContests = ({
 
 export const getContestById = ({ contestId }) =>
   http.get(`contests/${contestId}`);
+
+export const getTransaction = () => http.get('users/id/transactions')
